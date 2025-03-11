eb5baf07a5bd54ee62e972bbf6ba4b939023e994
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-03-11 18:05:51 +0200

    add base template

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/lionpuro/portfolio
 
 go 1.24.1
+
+require (
+	github.com/a-h/templ v0.3.833
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	_ "github.com/joho/godotenv/autoload"
+	"github.com/lionpuro/portfolio/views"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /", indexHandler)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: mux,
+	}
+
+	fmt.Printf("Listening on port %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	views.Base().Render(r.Context(), w)
+}
