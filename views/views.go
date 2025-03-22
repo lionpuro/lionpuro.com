22cd4c626c3b95b4d97e7d133b86415b39335ab1
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-03-22 11:45:04 +0200

    add email link

--- views/views.go
+++ views/views.go
@@ -3,4 +3,5 @@ package views
 const (
 	github_url   = "https://github.com/lionpuro"
 	linkedin_url = "https://www.linkedin.com/in/lion-puro-09bb4b1a1/"
+	email        = "mailto:connect@lionpuro.com"
 )
