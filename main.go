5e668e7f328bbd1fc3f976e6a2dfafaeebcd20a7
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-03-25 15:48:20 +0200

    fix locale prefix breaking blog routing

--- main.go
+++ main.go
@@ -20,23 +20,47 @@ func main() {
 	if err := ctxi18n.Load(locales.Content); err != nil {
 		log.Fatalf("error loading locales: %v", err)
 	}
-	mux := http.NewServeMux()
+	rootRouter := http.NewServeMux()
 
-	pageMux := http.NewServeMux()
-	pageMux.HandleFunc("GET /", indexHandler)
-	pageMux.HandleFunc("GET /blog", blogHandler)
-	pageMux.HandleFunc("GET /blog/{slug}", postHandler)
+	pageRouter := http.NewServeMux()
 
-	mux.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
-	mux.HandleFunc("/", languageMiddleware(pageMux))
+	routes := map[string]http.HandlerFunc{
+		"GET /":            indexHandler,
+		"GET /blog":        blogHandler,
+		"GET /blog/{slug}": postHandler,
+	}
+	registerRoutes(pageRouter, routes)
+
+	rootRouter.Handle("GET /static/", http.StripPrefix("/static", http.FileServer(http.Dir("static"))))
+
+	rootRouter.HandleFunc("/", removeTrailingSlash(
+		languageMiddleware(pageRouter),
+	))
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%s", port),
-		Handler: gzipHandler(mux),
+		Handler: gzipHandler(rootRouter),
 	}
 
 	fmt.Printf("Listening on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func registerRoutes(router *http.ServeMux, routes map[string]http.HandlerFunc) {
+	for pattern := range routes {
+		router.HandleFunc(pattern, routes[pattern])
+		router.HandleFunc(withBase("fi", pattern), routes[pattern])
+		router.HandleFunc(withBase("en", pattern), routes[pattern])
+	}
+}
+
+func withBase(base, pattern string) string {
+	parts := strings.Fields(pattern)
+	if len(parts) == 1 {
+		return path.Join("/", base, parts[0])
+	}
+	newPath := path.Join("/", base, parts[1])
+	return parts[0] + " " + newPath
 }
 
 func detectLanguage(r *http.Request) string {
@@ -81,5 +105,19 @@ func languageMiddleware(next http.Handler) http.HandlerFunc {
 			return
 		}
 		next.ServeHTTP(w, r.WithContext(ctx))
+	}
+}
+
+func removeTrailingSlash(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL
+		path := url.Path
+		l := len(path) - 1
+		if l > 0 && strings.HasSuffix(path, "/") {
+			url := path[:l]
+			http.Redirect(w, r, url, http.StatusMovedPermanently)
+			return
+		}
+		next.ServeHTTP(w, r)
 	}
 }
