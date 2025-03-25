cb9ea3c90bde8618c1d41812ba71af2ffd4c4df2
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-03-25 18:59:11 +0200

    handle 404

--- handler.go
+++ handler.go
@@ -6,11 +6,17 @@ import (
 	"net/http"
 
 	"github.com/a-h/templ"
+	"github.com/invopop/ctxi18n/i18n"
 	"github.com/lionpuro/portfolio/blog"
 	"github.com/lionpuro/portfolio/views"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+	if p != "/" && p != "/fi" && p != "/en" {
+		notFoundHandler(w, r)
+		return
+	}
 	views.FullPage(views.Home(), "Lion Puro", "A full stack web developer").Render(r.Context(), w)
 }
 
@@ -27,11 +33,19 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	slug := r.PathValue("slug")
 	post, err := blog.ParsePost(slug)
 	if err != nil {
-		http.Redirect(w, r, "/", http.StatusNotFound)
+		notFoundHandler(w, r)
 		return
 	}
 	content := unsafe(post.Content)
 	views.FullPage(views.Post(post.Title, post.Date, content), "Lion Puro", "Blog").Render(r.Context(), w)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	msg := i18n.T(r.Context(), "not-found")
+	views.FullPage(
+		views.ErrorPage(http.StatusNotFound, msg), msg, "",
+	).Render(r.Context(), w)
 }
 
 func unsafe(html string) templ.Component {
