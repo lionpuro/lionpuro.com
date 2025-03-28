e06ee55830e44025a690006b6384b8911f5c1ded
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-03-28 11:43:07 +0200

    localize dates

--- locales/locales.go
+++ locales/locales.go
@@ -3,6 +3,9 @@ package locales
 import (
 	"context"
 	"embed"
+	"fmt"
+	"time"
+
 	"github.com/invopop/ctxi18n/i18n"
 )
 
@@ -11,4 +14,29 @@ var Content embed.FS
 
 func LocaleCode(ctx context.Context) string {
 	return i18n.GetLocale(ctx).Code().String()
+}
+
+var finnishMonths = map[string]string{
+	"January":   "Tammikuu",
+	"February":  "Helmikuu",
+	"March":     "Maaliskuu",
+	"April":     "Huhtikuu",
+	"May":       "Toukokuu",
+	"June":      "Kesäkuu",
+	"July":      "Heinäkuu",
+	"August":    "Elokuu",
+	"September": "Syyskuu",
+	"October":   "Lokakuu",
+	"November":  "Marraskuu",
+	"December":  "Joulukuu",
+}
+
+func DateString(ctx context.Context, t time.Time) string {
+	l := i18n.GetLocale(ctx).Code().String()
+	if l == "fi" {
+		y, _, d := t.Date()
+		m := finnishMonths[t.Month().String()]
+		return fmt.Sprintf("%d. %sta %d", d, m, y)
+	}
+	return t.Format("January _2, 2006")
 }
