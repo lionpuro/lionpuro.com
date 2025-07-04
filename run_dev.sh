c1fd6532c8e1a0b40b1cbae47ff238ced2538b53
Lion Puro <167165944+lionpuro@users.noreply.github.com>
2025-07-04 11:23:02 +0300

    remove unnecessary cleanup function

--- run_dev.sh
+++ run_dev.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-cleanup() {
-    echo "Cleaning up..."
-    exit
-}
-
-trap cleanup EXIT
-
 go tool wgo -debounce 100ms -xfile _templ.go -xdir assets/public \
 	npm run build \
 	:: wgo -xfile _templ.go -xdir assets/public templ generate views \
